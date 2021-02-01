c67699fb2f4daae8cc56c7b0dbb66f50de72778f
Kyle Smith <kysmith@linkedin.com>
2021-02-01 08:51:19 -0800

    Add path and source information to arg-type automatically

--- addon/helpers/arg-type.js
+++ addon/helpers/arg-type.js
@@ -2,11 +2,11 @@ import { helper } from '@ember/component/helper';
 import { assert } from '@ember/debug';
 import { createContextPath, ensureValidator } from '../-private/utils';
 
-export function argType([value, validator], { path }) {
-  const error = ensureValidator(validator)(value, createContextPath(path));
+export function argType([value, validator], { _path, _source }) {
+  const error = ensureValidator(validator)(value, createContextPath(_path));
   if (error) {
     const [message, context] = error;
-    assert(`helper:arg-type |> Property validation failure for argument "${context()}" |> ${message}`);
+    assert(`${_source} |> helper:arg-type |> Property validation failure for argument "${context()}" |> ${message}`);
   }
 }
 
